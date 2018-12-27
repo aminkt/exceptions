065ef8f952b3083a838d4e57cc57aab2ecf64672
Amin Keshavarz <ak_1596@yahoo.com>
2018-12-27 12:41:25 +0330

    Add parsian gate.

---
+++ src/NotImplementedException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace aminkt\exceptions;
+
+/**
+ * Class NotImplementedException
+ * Used to show a method or block of code should implement but not implemented.
+ *
+ * @author Amin Keshavarz <ak_1596@yahoo.com>
+ * @package aminkt\payment\exceptions
+ */
+class NotImplementedException extends \RuntimeException
+{
+
+}
